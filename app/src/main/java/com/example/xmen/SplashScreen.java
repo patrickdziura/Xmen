226f5d9e5b003d591a0253c24856354a0487efc6
patrickdziura@gmail.com <Naruto_1996>
2019-06-05 22:25:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/xmen/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.xmen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity implements Runnable {
+    private final int DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler h = new Handler();
+        h.postDelayed(this, DELAY);
+    }
+
+    @Override
+    public void run() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}

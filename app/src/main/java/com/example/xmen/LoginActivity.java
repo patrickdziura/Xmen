705808dd2db5735a3b123a804ab39f6aa7b53784
patrickdziura@gmail.com <Naruto_1996>
2019-06-12 02:03:07 -0300

    Testando Login e WS, quase lá

--- app/src/main/java/com/example/xmen/LoginActivity.java
+++ app/src/main/java/com/example/xmen/LoginActivity.java
@@ -2,12 +2,111 @@ package com.example.xmen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements Response.Listener, Response.ErrorListener{
+
+    public static final String REQUEST_TAG = "MainVolleyActivity";
+    private RequestQueue mQueue;
+    private EditText login;
+    private EditText senha;
+    private Button mButton;
+    private String url = "http://192.168.42.253:8080/Server/testLogin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (EditText) findViewById(R.id.editLogin);
+        senha = (EditText) findViewById(R.id.editSenha);
+        mButton = (Button) findViewById(R.id.btnLogin);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        final String user = login.getText().toString();
+        final String password = senha.getText().toString();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                        //parseData(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("login", user);
+                params.put("senha", password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mQueue = CustomVolleyRequestQueue.getInstance(this.getApplicationContext()).getRequestQueue();
+
+        final CustomJSONObjectRequest jsonRequest = new CustomJSONObjectRequest(Request.Method.POST, url,
+                new JSONObject(), this, this);
+        jsonRequest.setTag(REQUEST_TAG);
+
+        /*mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQueue.add(jsonRequest);
+            }
+        });*/
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mQueue != null) {
+            mQueue.cancelAll(REQUEST_TAG);
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        error.printStackTrace();
+    }
+
+    @Override
+    public void onResponse(Object response) {
+        System.out.println("Deu boa");
     }
 }
